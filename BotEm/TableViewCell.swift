ffefe0c7709745abbcf5fe6d86e784dafb93885a
Rachel Wurmbrand <rewurmbrand@gmail.com>
2019-04-04 18:39:41 -0700

    Initial Commit

---
+++ BotEm/TableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.swift
+//  BotEm
+//
+//  Created by RACHEL WURMBRAND on 4/4/19.
+//  Copyright © 2019 RACHEL WURMBRAND. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
