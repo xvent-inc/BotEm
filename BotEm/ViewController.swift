9bc97a8a992e6c39e6a911a67a3859b5c8fb9b9d
Rachel Wurmbrand <rewurmbrand@gmail.com>
2019-04-04 18:36:58 -0700

    Initial Commit

---
+++ BotEm/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BotEm
+//
+//  Created by RACHEL WURMBRAND on 4/4/19.
+//  Copyright © 2019 RACHEL WURMBRAND. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
